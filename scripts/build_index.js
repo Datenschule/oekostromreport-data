899d8ccbdf05d654a67d2f4944ba53064f53f1fc
lislis <mail@lislis.de>
2019-10-28 16:43:46 +0100

    use prebuild index

---
+++ scripts/build_index.js
@@ -0,0 +1,29 @@
+var lunr = require('lunr'),
+    stdin = process.stdin,
+    stdout = process.stdout,
+    buffer = [],
+    documents = {}
+
+stdin.resume()
+stdin.setEncoding('utf8')
+
+stdin.on('data', function (data) {
+  buffer.push(data)
+})
+
+stdin.on('end', function () {
+  documents = JSON.parse(buffer.join(''))
+
+  var idx = lunr(function () {
+    this.ref('index')
+    this.field('Firmenname')
+    this.field('URL');
+
+    documents.forEach((doc, i) => {
+      doc.index = i;
+      this.add(doc)
+    }, this)
+  })
+
+  stdout.write(JSON.stringify({idx: idx, store: documents}))
+})
